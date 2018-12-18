d35b26511baf210c920e36a8f68542686612f4d8
SauceBoss1 <dtvsworld@gmail.com>
2018-12-17 19:56:58 -0800

    Recursion help
    hope this helps

--- Misc Finals student help/src/misc_code.java
+++ Misc Finals student help/src/misc_code.java
@@ -26,9 +26,10 @@ public class misc_code {
 	
 	public static int choose_v2(int n, int k) {
 		if(k<0 || k>n) {
-			a+=1;
+			
 			return 0;
 		} else if( n == 0) {
+			a+=1;
 			return 1;
 		} else {
 			a+=1;
